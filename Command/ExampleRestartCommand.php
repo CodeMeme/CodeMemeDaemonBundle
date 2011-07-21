bbf3d6dbf1a6d42137a2b09caf2b786a414000a1
Jesse Greathouse <jesse.greathouse@gmail.com>
2011-07-21 09:12:50 -0500

    restart command uses restart method instead of manual start-stop

--- Command/ExampleRestartCommand.php
+++ Command/ExampleRestartCommand.php
@@ -26,10 +26,8 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $daemon = new Daemon($this->getContainer()->getParameter('example.daemon.options'));
-        $daemon->stop();
 
-        $daemon->iterate(5);
-        $daemon->start();
+        $daemon->reStart();
         
         while ($daemon->isRunning()) {
             $this->getContainer()->get('example.control')->run();
